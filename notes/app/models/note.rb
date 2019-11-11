71771f4626684f22cc02dee755964e8b4eea3032
jweath17 <jweath17@uncc.edu>
2019-11-11 20:48:14 +0000

    Added user/sesionns

--- notes/app/models/note.rb
+++ notes/app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ApplicationRecord
+    belongs_to :user
+    
     validates :title, presence: true
     validates :text, presence: true, length: {minimum: 5 }
 end
--- notes/db/schema.rb
+++ notes/db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_030603) do
+ActiveRecord::Schema.define(version: 2019_11_11_193821) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
