1ff0cde836714e1208c5aec8ac5fdc514de80d76
jweath17 <jweath17@uncc.edu>
2019-11-11 21:27:41 +0000

    Continued working on user sessions, made some arch changes

--- notes/db/schema.rb
+++ notes/db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_193821) do
+ActiveRecord::Schema.define(version: 2019_11_11_191406) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
