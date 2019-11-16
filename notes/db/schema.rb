4fc55ab1324ea023032832b8b24529b1a8a40e69
jweath17 <jweath17@uncc.edu>
2019-11-16 16:27:07 +0000

    Installed Cucumber

--- notes/db/schema.rb
+++ notes/db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_124557) do
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_notes_on_user_id"
